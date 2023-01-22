460f175e46b6893eda433946258592950001a7aa
Jakhongir611 <khurramovjahongir611@gmail.com>
2023-01-22 11:32:37 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,104 @@
+let salarues = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250,
+};
+
+let names = ['Bob','Jane','Alex','Dick']
+let total = names.concat(Object.keys(salarues))
+total.sort()
+
+
+console.log(total);
+
+
+let mas_name_1 = prompt('Введите любые имена')
+let mas_name_2 = prompt('Введите любые имена')
+let mas_name_3 = prompt('Введите любые имена')
+let mas_name_4 = prompt('Введите любые имена')
+let mas_name_5 = prompt('Введите любые имена')
+let mas_name_6 = prompt('Введите любые имена')
+let mas_name_7 = prompt('Введите любые имена')
+let mas_name_8 = prompt('Введите любые имена')
+let mas_name_9 = prompt('Введите любые имена')
+let mas_name_10 = prompt('Введите любые имена')
+
+
+let prompt_names = [mas_name_1, mas_name_2, mas_name_3, mas_name_4, mas_name_5,mas_name_6, mas_name_7,mas_name_8,mas_name_9,mas_name_10]
+
+prompt_names.sort()
+
+console.log(prompt_names);
+
+
+
+
+
+
+
+// let arr = [[{a: {price: 20}}][{a: {price: 35}}][{a: {price: 44}}]]
+// let total = 0
+// arr = arr.flat()
+// let str = arr.split
+
+// console.log(arr);
+
+
+
+
+
+// let arr = [1,2,3,4,5,6,7,8,9] //
+
+// arr.push(10) //Добавить с конец
+// arr.unshift(0) //Добавить с начало
+// arr.unshift('str')
+// arr.push(true)
+
+
+
+// //удаление элеменов
+// arr.pop() //Удаление с конца
+// arr.shift() //Удаление с начало
+
+
+// let ctrl_c = arr.slice(3,5)
+
+// let ctrl_x = arr.splice(3,5)
+
+// console.log(ctrl_c);
+// console.log(ctrl_x);
+
+// let concat = arr.concat(ctrl_x)
+
+
+// let namesd = ['Joxa', 'axmed', 'shoxa', ]
+
+// names.sort((a, b,)=>{
+//     return a - b
+// })
+
+// console.log(namesd);
+// // console.log(concat);
+
+
+
+
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+// let obj = {
+//     name: "Joxa",
+//     age: 20,
+// }
+// obj.nickname = "joxa611"
+// delete obj.age
+// console.log(obj);
